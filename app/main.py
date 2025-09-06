ad4fe99372fd582bf606759a230bbc7d7e5c4a96
Vishwaraj <vishu.sankla@gmail.com>
2025-09-06 12:20:34 +0530

    msg

--- app/main.py
+++ app/main.py
@@ -8,4 +8,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))  # fallback for local dev
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
